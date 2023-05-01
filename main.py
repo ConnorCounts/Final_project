eb971c3fdd5ae4f8a1194a5f3931b466c75d1b3c
ConnorCounts <connorjcounts@gmail.com>
2023-05-01 11:57:24 -0700

    Layout of Final Game

---
+++ main.py
@@ -0,0 +1,128 @@
+# By Connor Counts
+
+"""
+Game structure:
+GOALS; RULES; FEEDBACK; FREEDOM
+
+My goal is:
+For this final project, I want to make a mario like game where the user has to jump on the platforms to reach a goal.
+I am using the color changing platforms for this game too, and I want to make it so if the user falls off of a platform on to the ground, the game restarts. 
+
+"""
+
+
+# import libraries
+import pygame as pg
+import os
+# allowing the  Setting and Sprites files to be used
+from settings import *
+from sprites import *
+# from pg.sprite import Sprite
+
+# set up assets folders
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "img")
+
+# create game class in order to pass properties to the sprite file
+class Game: 
+    def __init__(self):
+        # init game window etc.
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("Final game")
+        self.clock = pg.time.Clock()
+        self.running = True
+        print(self.screen)
+    
+    # starting a new game
+    def new(self):
+
+        # setting up classes to self.__ to be used easier in the code
+        self.all_sprites = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
+        self.enemies = pg.sprite.Group()
+        self.player = Player(self)
+
+            #self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (50,50,50), "normal")
+            #self.all_sprites.add(self.plat1)
+            #self.platforms.add(self.plat1)
+
+        #adding the platforms to the game 
+        self.all_sprites.add(self.player)
+        for plat in PLATFORM_LIST:
+            p = Platform(*plat)
+            self.all_sprites.add(p)
+            self.platforms.add(p)
+        
+        self.run()
+
+    # while the game is going on events, update, and draw are running too
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    # When the user quits the game, the game will sop running and when the user clicks the space bar, the player will jump
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.player.jump()
+           
+
+    def update(self):
+        self.all_sprites.update()
+
+        # if the player is falling
+        if self.player.vel.y > 0:
+            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+
+            # checking to see if the player has collided with a platform
+            # if the player hits a platform, the color of the platform will change to red
+            if hits:
+                self.player.pos.y = hits[0].rect.top
+                self.player.vel.y = 0
+                print ("landed!!!!")
+                hits[0].image.fill(RED)
+            else:
+                # if the player is not on a platform, the platform will go back to white
+                self.player.standing = False
+                    #self.platforms[0].image.fill(WHITE)
+                for platform in self.platforms:
+                    platform.image.fill(WHITE)
+
+    # Making the screen blue
+    def draw (self):
+        self.screen.fill(BLUE)
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    # Making paramaters for allowing text to be drawn on the screen
+    def draw_text(self, text, size, color, x, y):
+        font_name = pg.font.match_font('arial')
+        font = pg.font.Font(font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x,y)
+        self.screen.blit(text_surface, text_rect)
+
+    def get_mouse_now(self):
+        x,y = pg.mouse.get_pos()
+        return (x,y)
+ 
+# instantiate the game class
+g = Game()
+
+# kick off the game loop
+while g.running:
+    g.new()
+
+pg.quit()
---
+++ sprites.py
@@ -0,0 +1,91 @@
+# importing the libraries to use
+import pygame as pg
+# Allowing this sprite file to go into the main file and allowing the Settings file to work in with this file 
+from pygame.sprite import Sprite
+from settings import *
+from random import randint 
+
+vec = pg.math.Vector2
+
+# player class
+class Player(Sprite):
+    def __init__(self, game):
+        Sprite.__init__(self)
+        # Setting the properties for the player
+        self.game = game
+        self.image = pg.Surface((50,50))
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+        self.cofric = 0.1
+        self.canjump = False
+        self.standing = False
+        self.jumping = False
+
+    def input(self):
+        keystate = pg.key.get_pressed()
+        if keystate[pg.K_a]:
+            self.acc.x = -PLAYER_ACC
+        if keystate[pg.K_d]:
+            self.acc.x = PLAYER_ACC
+       
+    # defining how the player can jump 
+    def jump(self):
+        self.jumping = True
+        self.rect.x += 1
+        hits = pg.sprite.spritecollide(self, self.game.platforms, False)
+        self.rect.x -= 1
+        #if hits:
+        self.vel.y = -PLAYER_JUMP
+        self.jumping = False
+
+    # 
+    def update(self):
+        self.acc = vec(0, PLAYER_GRAV)
+        self.acc.x = self.vel.x * PLAYER_FRICTION
+        self.input()
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        self.rect.bottomleft = self.pos
+        
+        # trying to create boundaries for my last game design
+
+        # the step is at 550
+        #if (self.pos[0] < 560 and self.pos[0] > 200):
+            
+            #if self.pos[1] > 500:     
+        print(self.pos)
+
+#class for a mob, but is not being used for this game
+class Mob(Sprite):
+    def __init__(self,width,height, color):
+        Sprite.__init__(self)
+        self.width = width
+        self.height = height
+        self.image = pg.Surface((self.width,self.height))
+        self.color = color
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(randint(1,5),randint(1,5))
+        self.acc = vec(1,1)
+        self.cofric = 0.01
+
+# creating a platform class
+class Platform(Sprite):
+    def __init__(self, x, y, width, height, color, variant):
+        Sprite.__init__(self)
+        self.width = width
+        self.height = height
+        self.image = pg.Surface((self.width,self.height))
+        self.color = color
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.variant = variant
+        
